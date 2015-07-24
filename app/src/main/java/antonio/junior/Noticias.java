028c3477f3034628ed64f9dbf529a5d4886aee71
Antonio <antoniojsh93@gmail.com>
2015-07-24 18:08:29 -0500

    Lazy Load Correctamente

--- app/src/main/java/antonio/junior/Noticias.java
+++ app/src/main/java/antonio/junior/Noticias.java
@@ -3,73 +3,119 @@ package antonio.junior;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceActivity;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
-
 import org.apache.http.Header;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
+import com.loopj.android.http.*;
+
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
 
 public class Noticias extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
     SwipeRefreshLayout swipeLayout;
     ListView list;
-    //LazyImageLoadAdapter adapter;
+    LazyAdapter adapter;
+
+    String titulo = "";
+    String Descripcion = "";
+    String urlImage = "";
+    String titulos[]=null;
+    List<String> Parrafo = new ArrayList<String>();
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_noticias, container, false);
+        super.onActivityCreated(savedInstanceState);
 
+        View rootView = inflater.inflate(R.layout.fragment_noticias, container, false);
 
         swipeLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.layoutnoticias);
         swipeLayout.setOnRefreshListener(this);
-      /*list= (ListView) rootView.findViewById(R.id.list);
 
-        adapter= new LazyImageLoadAdapter(this.getActivity(),strings);
-        list.setAdapter(adapter);*/
-       loadFeed();
+        list = (ListView) rootView.findViewById(R.id.list);
+        Button b = (Button) rootView.findViewById(R.id.button1);
+        b.setOnClickListener(listener);
+        loadFeed();
         return rootView;
     }
 
+
+
+    @Override
+    public void onDestroy()
+    {
+        list.setAdapter(null);
+        super.onDestroy();
+    }
+
+    public View.OnClickListener listener=new View.OnClickListener(){
+        @Override
+        public void onClick(View arg0) {
+            adapter.imageLoader.clearCache();
+            adapter.notifyDataSetChanged();
+        }
+    };
+
     private void loadFeed() {
-        ProgressDialog progress;
-        progress = ProgressDialog.show(this.getActivity(), "Cargando",
-                "Espera Miestras se cargan las noticias", true);
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get("http://juniorfc.co/feed/", new AsyncHttpResponseHandler() {
 
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                if (statusCode == 200) {
-                    String result = new String(responseBody);
-                    Log.d("************ ", "TODO SALIO BIEN");
-                    parserXML(result);
-                }
-            }
+       AsyncHttpClient client = new AsyncHttpClient();
+       client.get("http://juniorfc.co/feed/", new AsyncHttpResponseHandler() {
 
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                Log.d("************ ","TODO SALIO MAL");
-            }
+           @Override
+           public void onStart() {
+               // called before request is started
+           }
 
-        });
-        progress.dismiss();
+           @Override
+           public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+               if (statusCode == 200) {
+                   String result = new String(responseBody);
+                   if (result != "") {
+
+                       parserXML(result);
+
+                   }
+
+               }
+           }
+
+           @Override
+           public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+           }
+
+       });
+
+
     }
 
     private void parserXML(String result) {
 
+        Integer cont=0;
         try {
             XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
             factory.setNamespaceAware(true);
@@ -77,56 +123,72 @@ public class Noticias extends Fragment implements SwipeRefreshLayout.OnRefreshLi
             xpp.setInput(new StringReader(result));
             String value = null;
 
-            while(xpp.getEventType() !=XmlPullParser.END_DOCUMENT){ // loop from the beginning to the end of the XML document
-                String titulo="";
-                String Descripcion="";
-                if(xpp.getEventType()==XmlPullParser.START_TAG){
+            int i = 0;
 
-                    if(xpp.getName().equals("title")){
-                        //Toast.makeText(this.getActivity(), xpp.nextText(), Toast.LENGTH_LONG).show();
-                        titulo =xpp.nextText().toString();
-                    }
-                    if(xpp.getName().equals("encoded")){
+            while (xpp.getEventType() != XmlPullParser.END_DOCUMENT) {
 
-                        //String[] separated = android.text.Html.fromHtml(xpp.nextText()).toString().split("Read More");
-                        String [] imgTemp = xpp.nextText().toString().split("href=\"");
-                        String [] imgFinal = imgTemp[1].split("\">");
-                        //Descripcion= separated[0].toString();
-                        Toast.makeText(this.getActivity(),imgFinal[0], Toast.LENGTH_LONG).show();
-                    }
+               Parrafo.add("Holaaaa "+i);
 
-
-                }
-                else if(xpp.getEventType()==XmlPullParser.END_TAG){
-
-                    // ... end of the tag: </version> in our example
-                }
-                else if(xpp.getEventType()==XmlPullParser.TEXT){ // in a text node
-                    value = xpp.getText(); // here you get the "1" value
-                }
-
+                i++;
                 xpp.next(); // next XPP state
             }
+
         } catch (XmlPullParserException e) {
 
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        adapter = new LazyAdapter(this.getActivity(), mStrings,Parrafo);
+        list.setAdapter(adapter);
 
     }
-
 
     @Override
     public void onRefresh() {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+               
                 swipeLayout.setRefreshing(false);
+                adapter.imageLoader.clearCache();
+                adapter.notifyDataSetChanged();
             }
         }, 5000);
     }
 
-
-        }
+    private String[] mStrings = {
+            "http://androidexample.com/media/webservice/LazyListView_images/image0.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image1.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image2.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image3.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image4.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image5.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image6.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image7.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image8.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image9.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image10.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image0.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image1.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image2.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image3.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image4.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image5.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image6.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image7.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image8.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image9.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image10.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image0.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image1.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image2.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image3.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image4.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image5.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image6.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image7.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image8.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image9.png",
+            "http://androidexample.com/media/webservice/LazyListView_images/image10.png" };
+}
